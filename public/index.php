2d011e1c9faddeb94a337a8892615f0c2a9b5451
RezguaniHiba <hibarezguani012@gmail.com>
2025-08-11 19:50:01 +0100

    Final commit

--- public/index.php
+++ public/index.php
@@ -16,7 +16,5 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 
 $app->handleRequest(Request::capture());
